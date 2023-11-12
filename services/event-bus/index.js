7220ced072f83465454afaa20dd93125e5e083df
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-11-12 18:04:25 -0500

    tested posts service k8 deployment with event bus k8 deployment successfully

--- services/event-bus/index.js
+++ services/event-bus/index.js
@@ -34,18 +34,18 @@ app.post('/events', (req, res) => {
     // add event to event store
     eventsStore.push(event);
 
-        axios.post('http://localhost:4000/events', event).catch(err => {
+        axios.post('http://posts-clusterip-srv:4000/events', event).catch(err => {
             console.log(err.message);
         });
-        axios.post('http://localhost:4001/events', event).catch(err => {
-            console.log(err.message);
-        });
-        axios.post('http://localhost:4002/events', event).catch(err => {
-            console.log(err.message);
-        });
-        axios.post('http://localhost:4003/events', event).catch(err => {
-            console.log(err.message);
-        });
+        // axios.post('http://localhost:4001/events', event).catch(err => {
+        //     console.log(err.message);
+        // });
+        // axios.post('http://localhost:4002/events', event).catch(err => {
+        //     console.log(err.message);
+        // });
+        // axios.post('http://localhost:4003/events', event).catch(err => {
+        //     console.log(err.message);
+        // });
 
         res.status(200).send({status: 'OK'});
 });
